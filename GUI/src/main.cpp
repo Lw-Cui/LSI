89b84ebb84b50fcda4db85eb5e0dc90971a19bb5
Liwei <cui@hellolw.com>
2016-11-02 16:05:28 +0800

    完成基本的文本窗口和绘图窗口

--- GUI/src/main.cpp
+++ GUI/src/main.cpp
@@ -1,22 +1,51 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
 
 int main() {
+    sf::RenderWindow drawingBoard(sf::VideoMode(1300, 1300), "Drawing Board"),
+            textWindow(sf::VideoMode(1100, 800), "Shell");
+    drawingBoard.setPosition(sf::Vector2i{1100, 0});
+    textWindow.setPosition(sf::Vector2i{0, 0});
+    drawingBoard.setVerticalSyncEnabled(true);
+    textWindow.setVerticalSyncEnabled(true);
 
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    std::string s;
 
-    while (window.isOpen()) {
+    sf::Font font;
+    font.loadFromFile("monaco.ttf");
+
+    sf::Text text;
+    text.setFont(font);
+    text.setCharacterSize(26);
+    text.setFillColor(sf::Color::Black);
+    text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+
+    while (textWindow.isOpen() && drawingBoard.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
+
+        while (textWindow.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                textWindow.close();
+            } else if (event.type == sf::Event::TextEntered) {
+                s.push_back(static_cast<char>(event.text.unicode));
+                text.setString(s);
+            }
+        }
+        while (drawingBoard.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                textWindow.close();
+            }
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+        textWindow.clear(sf::Color::White);
+        drawingBoard.clear(sf::Color::White);
+        textWindow.draw(text);
 
+        textWindow.display();
+        drawingBoard.display();
+    }
     return 0;
 }
+
