6c817d87b9480a598238d2c04f4e30c1897894d7
Liwei <cui@hellolw.com>
2016-11-06 21:41:39 +0800

    改进日志库的输出

--- test/main.cpp
+++ test/main.cpp
@@ -5,6 +5,12 @@ INITIALIZE_EASYLOGGINGPP
 
 int main(int argc, char *argv[]) {
     START_EASYLOGGINGPP(argc, argv);
+    el::Logger* lexerLogger = el::Loggers::getLogger("lexer");
+    el::Logger* parserLogger = el::Loggers::getLogger("parser");
+    el::Logger* exceptionLogger = el::Loggers::getLogger("exception");
+    el::Loggers::reconfigureAllLoggers(el::ConfigurationType::Format,
+                                       "[%logger][%func] %msg [%file:%line]");
+
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
