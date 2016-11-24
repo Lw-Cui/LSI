a6375e9552c14a276e2fd85f5a6843baf93e5635
Liwei <cui@hellolw.com>
2016-11-24 15:35:34 +0800

    改动lambda 在被调用时 identifier 被查找的方式

--- Interpreter/lib/Base.scm
+++ Interpreter/lib/Base.scm
@@ -1,3 +1,17 @@
+(define (and expr . args)
+    (define (list-and l)
+        (if (null? l) #t (if (car l) (list-and (cdr l)) #f)))
+    (if expr (list-and args) #f))
+
+(define (not expr) (if expr #f #t))
+
+(define (= expr . args)
+    (define (equal x y)
+        (and (not (< x y)) (not (< y x))))
+    (define (equal-list l)
+        (if (null? (cdr l)) #t (if (equal (car l) (car (cdr l))) (equal-list (cdr l)) #f)))
+    (if (equal expr (car args)) (equal-list args) #f))
+
 (define (- num . args)
     (define (add-list l)
         (if (null? l) 0 (+ (car l) (add-list (cdr l)))))
@@ -10,25 +24,11 @@
         (if (null? l) 1 (* (car l) (multiply-list (cdr l)))))
     (* num (#reciprocal (multiply-list args))))
 
-(define (not expr) (if expr #f #t))
-
-(define (and expr . args)
-    (define (list-and l)
-        (if (null? l) #t (if (car l) (list-and (cdr l)) #f)))
-    (if expr (list-and args) #f))
 
 (define (or expr . args)
     (define (list-or l)
         (if (null? l) #f (if (car l) #t (list-or (cdr l)))))
     (if expr #t (list-or args)))
-
-(define (= expr . args)
-    (define (equal x y)
-        (and (not (< x y)) (not (< y x))))
-    (define (equal-list l)
-        (if (null? (cdr l)) #t (if (equal (car l) (car (cdr l))) (equal-list (cdr l)) #f)))
-    (if (equal expr (car args)) (equal-list args) #f))
-
 
 (define (remainder a b)
   (if (< a b)
