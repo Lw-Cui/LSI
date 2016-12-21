bc9c73d8dff65207ed9439ff0ef788e94d8ed991
Liwei <cui@hellolw.com>
2016-12-21 12:34:17 +0800

    继续增加 mock 测试

--- GUI/test/drawingBoardTest.cpp
+++ GUI/test/drawingBoardTest.cpp
@@ -27,3 +27,49 @@ TEST(DrawingBoardTest, drawPointTest) {
     controller.execute();
     controller.drawToWindows();
 }
+
+TEST(DrawingBoardTest, drawMultiplePointTest) {
+    testing::StrictMock<MockWindow> drawingBoard, textWindow;
+
+    con::VertexArray va;
+    va.append(Vertex{Vector2f(5, 5), Color::Black});
+    va.append(Vertex{Vector2f(50, 50), Color::Black});
+    va.append(Vertex{Vector2f(500, 500), Color::Black});
+    EXPECT_CALL(drawingBoard, draw(va)).Times(1);
+
+    Controller controller{textWindow, drawingBoard};
+    string code{"(draw (list (cons 5 5) (cons 50 50) (cons 500 500)))"};
+    for (char c: code) controller.appendChar(c);
+    controller.execute();
+    controller.drawToWindows();
+}
+
+TEST(DrawingBoardTest, drawLineTest) {
+    testing::StrictMock<MockWindow> drawingBoard, textWindow;
+
+    con::VertexArray va;
+    for (int i = 100; i > 4; i--)
+        va.append(Vertex{Vector2f(5, i), Color::Black});
+    EXPECT_CALL(drawingBoard, draw(va)).Times(1);
+
+    Controller controller{textWindow, drawingBoard};
+    string code{"(draw (line (cons 5 5) (cons 5 100)))"};
+    for (char c: code) controller.appendChar(c);
+    controller.execute();
+    controller.drawToWindows();
+}
+
+TEST(DrawingBoardTest, drawLineTestV2) {
+    testing::StrictMock<MockWindow> drawingBoard, textWindow;
+
+    con::VertexArray va;
+    for (int i = 100; i > 4; i--)
+        va.append(Vertex{Vector2f(i, i), Color::Black});
+    EXPECT_CALL(drawingBoard, draw(va)).Times(1);
+
+    Controller controller{textWindow, drawingBoard};
+    string code{"(draw (line (cons 5 5) (cons 100 100)))"};
+    for (char c: code) controller.appendChar(c);
+    controller.execute();
+    controller.drawToWindows();
+}
