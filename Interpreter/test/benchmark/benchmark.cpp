0874428c198592af3cc13f9f5c724ef57ceefd56
lwCui <cui@hellolw.com>
2017-01-17 08:57:59 +0800

    upgrade benchmark src

--- Interpreter/test/benchmark/benchmark.cpp
+++ Interpreter/test/benchmark/benchmark.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <parser.h>
 #include <exception.h>
+#include <easylogging++.h>
+
+INITIALIZE_EASYLOGGINGPP
 
 using namespace std;
 using namespace lexers;
@@ -9,12 +12,16 @@ using namespace exception;
 
 int main() {
     try {
+        el::Logger *parserLogger = el::Loggers::getLogger("parser");
+        el::Logger *exceptionLogger = el::Loggers::getLogger("exception");
+        el::Loggers::reconfigureAllLoggers(el::ConfigurationType::ToFile, "false");
+        el::Loggers::reconfigureAllLoggers(el::ConfigurationType::Format,
+                                                           "[%logger] %msg [%fbase:%line]");
         Scope s;
         lexers::Lexer lex;
         lex.appendExp("(load \"setup.scm\")").appendExp("(load \"benchmark.scm\")");
         lex.appendExp("#(length (l-painter default))");
-        lex.appendExp(
-                "(length ((beside (below painter (flip-vert painter)) (below painter (flip-vert painter))) default))");
+        lex.appendExp("(length ((beside (below painter (flip-vert painter)) (below painter (flip-vert painter))) default))");
         auto res = parseAllExpr(lex)->eval(s);
         auto numPtr = std::dynamic_pointer_cast<NumberAST>(res);
         if (numPtr) {
