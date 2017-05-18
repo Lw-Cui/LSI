02044b09b45f8a03a417cd9822c714dd76907212
Liwei <cui@hellolw.com>
2017-05-18 18:24:38 +0800

    modifying builtinFunctionTest

--- Interpreter/test/parser/builtinFunctionTest.cpp
+++ Interpreter/test/parser/builtinFunctionTest.cpp
@@ -5,181 +5,126 @@
 using namespace lexers;
 using namespace parser;
 
-TEST(BuiltinFunctionTest, BuiltinConsTest) {
-    auto s = std::make_shared<Scope>();
-    lexers::Lexer lex("(define p (cons 1 2))");
+#define CREATE_CONTEXT() \
+    auto s = std::make_shared<Scope>();\
+    lexers::Lexer lex;\
+    pExpr ast, res;\
+    std::shared_ptr<NumberAST> numPtr;\
+    std::shared_ptr<BooleansTrueAST> trueBool;\
+    std::shared_ptr<BooleansFalseAST> falseBool;
 
-    lex.appendExp("(cdr p)");
-    auto ast = parseAllExpr(lex);
-    auto res = ast->eval(s, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<NumberAST>(res));
-    auto numPtr = std::dynamic_pointer_cast<NumberAST>(res);
+#define TO_NUM_PTR(res)\
+    std::dynamic_pointer_cast<NumberAST>(res)
+
+#define TO_TRUE_PTR(res)\
+    std::dynamic_pointer_cast<BooleansTrueAST>(res)
+
+#define TO_FALSE_PTR(res)\
+    std::dynamic_pointer_cast<BooleansFalseAST>(res)
+
+#define REPL_COND(str, condition)\
+    lex.appendExp(str);\
+    ast = parseAllExpr(lex);\
+    res = ast->eval(s, ast);\
+    ASSERT_TRUE(condition);\
+    numPtr = TO_NUM_PTR(res);\
+    trueBool = TO_TRUE_PTR(res);\
+    falseBool = TO_FALSE_PTR(res);
+
+
+TEST(BuiltinFunctionTest, BuiltinConsTest) {
+    CREATE_CONTEXT();
+    lex.appendExp("(define p (cons 1 2))");
+    REPL_COND("(cdr p)", TO_NUM_PTR(res));
     ASSERT_EQ(2, numPtr->getValue());
     ASSERT_STREQ("2", numPtr->display().c_str());
 
-    lex.appendExp("(car p)");
-    ast = parseAllExpr(lex);
-    res = ast->eval(s, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<NumberAST>(res));
-    numPtr = std::dynamic_pointer_cast<NumberAST>(res);
+    REPL_COND("(car p)", TO_NUM_PTR(res));
     ASSERT_EQ(1, numPtr->getValue());
     ASSERT_STREQ("1", numPtr->display().c_str());
 
-    lex.appendExp("(define pp (cons 3 p))")
-            .appendExp("(car (cdr pp))");
-    ast = parseAllExpr(lex);
-    res = ast->eval(s, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<NumberAST>(res));
-    numPtr = std::dynamic_pointer_cast<NumberAST>(res);
+    lex.appendExp("(define pp (cons 3 p))");
+    REPL_COND("(car (cdr pp))", TO_NUM_PTR(res));
     ASSERT_EQ(1, numPtr->getValue());
     ASSERT_STREQ("1", numPtr->display().c_str());
 }
 
 TEST(BuiltinFunctionTest, NullTest) {
-    auto s = std::make_shared<Scope>();
-    lexers::Lexer lex("(define p (cons 1 nil))");
+    CREATE_CONTEXT();
+    lex.appendExp("(define p (cons 1 nil))");
 
-    lex.appendExp("(null? (cdr p))");
-    auto ast = parseAllExpr(lex);
-    auto res = ast->eval(s, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<BooleansTrueAST>(res));
-    auto trueBool = std::dynamic_pointer_cast<BooleansTrueAST>(res);
+    REPL_COND("(null? (cdr p))", TO_TRUE_PTR(res));
     ASSERT_STREQ("#t", trueBool->display().c_str());
 
-    lex.appendExp("(null? (car p))");
-    ast = parseAllExpr(lex);
-    res = ast->eval(s, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<BooleansFalseAST>(res));
-    auto falseBool = std::dynamic_pointer_cast<BooleansFalseAST>(res);
+    REPL_COND("(null? (car p))", TO_FALSE_PTR(res));
     ASSERT_STREQ("#f", falseBool->display().c_str());
 }
 
 TEST(BuiltinFunctionTest, AddTest) {
-    auto ss = std::make_shared<Scope>();
-    lexers::Lexer lex{"(+ 5 6 7)"};
-    auto ast = parseExpr(lex);
-    auto exprPtr = ast->eval(ss, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<NumberAST>(exprPtr));
-    auto numPtr = std::dynamic_pointer_cast<NumberAST>(exprPtr);
+    CREATE_CONTEXT();
+    REPL_COND("(+ 5 6 7)", TO_NUM_PTR(res));
     ASSERT_EQ(18, numPtr->getValue());
     ASSERT_STREQ("18", numPtr->display().c_str());
 }
 
 TEST(BuiltinFunctionTest, MultiplyTest) {
-    auto ss = std::make_shared<Scope>();
-    lexers::Lexer lex{"(* 5 6 7)"};
-    auto ast = parseExpr(lex);
-    auto exprPtr = ast->eval(ss, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<NumberAST>(exprPtr));
-    auto numPtr = std::dynamic_pointer_cast<NumberAST>(exprPtr);
+    CREATE_CONTEXT();
+    REPL_COND("(* 5 6 7)", TO_NUM_PTR(res));
     ASSERT_EQ(210, numPtr->getValue());
     ASSERT_STREQ("210", numPtr->display().c_str());
 }
 
 TEST(BuiltinFunctionTest, MinusTest) {
-    auto ss = std::make_shared<Scope>();
-
-    lexers::Lexer lex;
-    lex.appendExp("(load \"setup.scm\")").appendExp("(- 5)");
-    auto ast = parseAllExpr(lex);
-    auto exprPtr = ast->eval(ss, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<NumberAST>(exprPtr));
-    auto numPtr = std::dynamic_pointer_cast<NumberAST>(exprPtr);
+    CREATE_CONTEXT();
+    lex.appendExp("(load \"setup.scm\")");
+    REPL_COND("(- 5)", TO_NUM_PTR(res));
     ASSERT_EQ(-5, numPtr->getValue());
     ASSERT_STREQ("-5", numPtr->display().c_str());
 
-    lex.appendExp("(+ (- 8) 6 (+ 1))");
-    ast = parseExpr(lex);
-    exprPtr = ast->eval(ss, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<NumberAST>(exprPtr));
-    numPtr = std::dynamic_pointer_cast<NumberAST>(exprPtr);
+    REPL_COND("(+ (- 8) 6 (+ 1))", TO_NUM_PTR(res));
     ASSERT_EQ(-1, numPtr->getValue());
     ASSERT_STREQ("-1", numPtr->display().c_str());
 }
 
 TEST(BuiltinFunctionTest, LessThanTest) {
-    auto ss = std::make_shared<Scope>();
-    lexers::Lexer lex("(< 5 6 7)");
-    auto ast = parseExpr(lex);
-    auto exprPtr = ast->eval(ss, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<BooleansTrueAST>(exprPtr));
-    auto trueBool = std::dynamic_pointer_cast<BooleansTrueAST>(exprPtr);
+    CREATE_CONTEXT();
+    REPL_COND("(< 5 6 7)", TO_TRUE_PTR(res));
     ASSERT_STREQ("#t", trueBool->display().c_str());
 
     lex.appendExp("(define n 6)");
-    ast = parseExpr(lex);
-    ast->eval(ss, ast);
-
-    lex.appendExp("(< n 5)");
-    ast = parseExpr(lex);
-    exprPtr = ast->eval(ss, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<BooleansFalseAST>(exprPtr));
-    auto falseBool = std::dynamic_pointer_cast<BooleansFalseAST>(exprPtr);
+    REPL_COND("(< n 5)", TO_FALSE_PTR(res));
     ASSERT_STREQ("#f", falseBool->display().c_str());
 
-    lex.appendExp("(< 0 0)");
-    ast = parseAllExpr(lex);
-    exprPtr = ast->eval(ss, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<BooleansFalseAST>(exprPtr));
-    falseBool = std::dynamic_pointer_cast<BooleansFalseAST>(exprPtr);
+    REPL_COND("(< 0 0)", TO_FALSE_PTR(res));
     ASSERT_STREQ("#f", falseBool->display().c_str());
-
 }
 
 TEST(BuiltinFunctionTest, NilTest) {
-    auto ss = std::make_shared<Scope>();
-    lexers::Lexer lex;
-    lex.appendExp("(null? (list 1 2 3))");
-    auto ast = parseAllExpr(lex);
-    auto exprPtr = ast->eval(ss, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<BooleansFalseAST>(exprPtr));
+    CREATE_CONTEXT();
+    REPL_COND("(null? (list 1 2 3))", TO_FALSE_PTR(res));
 
-    lex.appendExp("(define l (list 1 2 3))").appendExp("(null? l)");
-    ast = parseAllExpr(lex);
-    exprPtr = ast->eval(ss, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<BooleansFalseAST>(exprPtr));
+    lex.appendExp("(define l (list 1 2 3))");
+    REPL_COND("(null? l)", TO_FALSE_PTR(res));
 
-    lex.appendExp("(null? nil)");
-    ast = parseAllExpr(lex);
-    exprPtr = ast->eval(ss, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<BooleansTrueAST>(exprPtr));
-
-    lex.appendExp("(define (add-list l) (if (null? l) #t #f))")
-            .appendExp("(add-list (list 5 6 7 8))");
-    ast = parseAllExpr(lex);
-    exprPtr = ast->eval(ss, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<BooleansFalseAST>(exprPtr));
+    lex.appendExp("(define (add-list l) (if (null? l) #t #f))");
+    REPL_COND("(add-list (list 5 6 7 8))", TO_FALSE_PTR(res));
 }
 
 TEST(BuiltinFunctionTest, ListTest) {
-    auto ss = std::make_shared<Scope>();
-    lexers::Lexer lex;
-    lex.appendExp("(define seq (list 1 2 3))").appendExp("(car seq)");
-    auto ast = parseAllExpr(lex);
-    auto exprPtr = ast->eval(ss, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<NumberAST>(exprPtr));
-    auto numPtr = std::dynamic_pointer_cast<NumberAST>(exprPtr);
+    CREATE_CONTEXT();
+    lex.appendExp("(define seq (list 1 2 3))");
+    REPL_COND("(car seq)", TO_NUM_PTR(res));
     ASSERT_EQ(1, numPtr->getValue());
     ASSERT_STREQ("1", numPtr->display().c_str());
 
-    lex.appendExp("(car (cdr seq))");
-    ast = parseAllExpr(lex);
-    exprPtr = ast->eval(ss, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<NumberAST>(exprPtr));
-    numPtr = std::dynamic_pointer_cast<NumberAST>(exprPtr);
+    REPL_COND("(car (cdr seq))", TO_NUM_PTR(res));
     ASSERT_EQ(2, numPtr->getValue());
     ASSERT_STREQ("2", numPtr->display().c_str());
 
-    lex.appendExp("(null? (cdr (cdr (cdr seq))))");
-    ast = parseAllExpr(lex);
-    exprPtr = ast->eval(ss, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<BooleansTrueAST>(exprPtr));
+    REPL_COND("(null? (cdr (cdr (cdr seq))))", TO_TRUE_PTR(res));
 
-    lex.appendExp("(define aseq (list 5 seq 6))").appendExp("(car (cdr (car (cdr aseq))))");
-    ast = parseAllExpr(lex);
-    exprPtr = ast->eval(ss, ast);
-    ASSERT_TRUE(std::dynamic_pointer_cast<NumberAST>(exprPtr));
-    numPtr = std::dynamic_pointer_cast<NumberAST>(exprPtr);
+    lex.appendExp("(define aseq (list 5 seq 6))");
+    REPL_COND("(car (cdr (car (cdr aseq))))", TO_NUM_PTR(res));
     ASSERT_EQ(2, numPtr->getValue());
     ASSERT_STREQ("2", numPtr->display().c_str());
 }
