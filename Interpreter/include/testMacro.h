21dd992ccc85b96518be6d5e9be37f4df9e7a705
Liwei <cui@hellolw.com>
2017-05-18 19:07:12 +0800

    add macro cpp

---
+++ Interpreter/include/testMacro.h
@@ -0,0 +1,30 @@
+#ifndef GI_TEST_MACRO_H
+#define GI_TEST_MACRO_H
+
+#define CREATE_CONTEXT() \
+    auto s = std::make_shared<Scope>();\
+    lexers::Lexer lex;\
+    pExpr ast, res;\
+    std::shared_ptr<NumberAST> numPtr;\
+    std::shared_ptr<BooleansTrueAST> trueBool;\
+    std::shared_ptr<BooleansFalseAST> falseBool;
+
+#define TO_NUM_PTR(res)\
+    std::dynamic_pointer_cast<NumberAST>(res)
+
+#define TO_TRUE_PTR(res)\
+    std::dynamic_pointer_cast<BooleansTrueAST>(res)
+
+#define TO_FALSE_PTR(res)\
+    std::dynamic_pointer_cast<BooleansFalseAST>(res)
+
+#define REPL_COND(str, condition)\
+    lex.appendExp(str);\
+    ast = parseAllExpr(lex);\
+    res = ast->eval(s, ast);\
+    ASSERT_TRUE(condition);\
+    numPtr = TO_NUM_PTR(res);\
+    trueBool = TO_TRUE_PTR(res);\
+    falseBool = TO_FALSE_PTR(res);
+    
+#endif
--- Interpreter/test/parser/builtinFunctionTest.cpp
+++ Interpreter/test/parser/builtinFunctionTest.cpp
@@ -1,35 +1,10 @@
 #include <memory>
 #include <gtest/gtest.h>
 #include <parser.h>
+#include <testMacro.h>
 
 using namespace lexers;
 using namespace parser;
-
-#define CREATE_CONTEXT() \
-    auto s = std::make_shared<Scope>();\
-    lexers::Lexer lex;\
-    pExpr ast, res;\
-    std::shared_ptr<NumberAST> numPtr;\
-    std::shared_ptr<BooleansTrueAST> trueBool;\
-    std::shared_ptr<BooleansFalseAST> falseBool;
-
-#define TO_NUM_PTR(res)\
-    std::dynamic_pointer_cast<NumberAST>(res)
-
-#define TO_TRUE_PTR(res)\
-    std::dynamic_pointer_cast<BooleansTrueAST>(res)
-
-#define TO_FALSE_PTR(res)\
-    std::dynamic_pointer_cast<BooleansFalseAST>(res)
-
-#define REPL_COND(str, condition)\
-    lex.appendExp(str);\
-    ast = parseAllExpr(lex);\
-    res = ast->eval(s, ast);\
-    ASSERT_TRUE(condition);\
-    numPtr = TO_NUM_PTR(res);\
-    trueBool = TO_TRUE_PTR(res);\
-    falseBool = TO_FALSE_PTR(res);
 
 
 TEST(BuiltinFunctionTest, BuiltinConsTest) {
